f4a2af1432abcf32bc97b058ef11a2055ef2b4a1
sureshgadupu <sureshgkhyd@gmail.com>
2023-04-06 22:59:17 +1200

    first commit

---
+++ src/main/java/dev/fullstackcode/ghrelease/SpringbootGhReleaseApplication.java
@@ -0,0 +1,13 @@
+package dev.fullstackcode.ghrelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGhReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGhReleaseApplication.class, args);
+	}
+
+}
